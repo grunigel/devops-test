74a663a03af48f015cc297f50df4baca08189883
Vlad Ziziko <ziziko.vlad@gmail.com>
2017-09-20 16:58:02 +0300

    initial

---
+++ DevOpsTestApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DevOpsTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DevOpsTestApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevOpsTestApp.MvcApplication" Language="C#" %>
